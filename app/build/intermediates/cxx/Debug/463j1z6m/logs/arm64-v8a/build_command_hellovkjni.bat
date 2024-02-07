a90632e91abaad9119c0357a7e010df9cc85b53b
Vincent <vincentallan001@gmail.com>
2024-02-07 02:53:46 -0800

    First Commit

---
+++ app/build/intermediates/cxx/Debug/463j1z6m/logs/arm64-v8a/build_command_hellovkjni.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\user\\AndroidStudioProjects\\ComputerGraphics\\app\\.cxx\\Debug\\463j1z6m\\arm64-v8a" ^
+  hellovkjni
